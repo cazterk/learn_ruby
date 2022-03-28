7d7960d4f1c7cfc13e05f176f83c47f4654f49a7
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2022-03-28 13:42:19 +0200

    numbers

--- variables.rb
+++ variables.rb
@@ -1,4 +1,5 @@
 # variables 
+
 =begin 
     Variables are used to store data.
     Variables are declared with the keyword:
@@ -8,6 +9,7 @@
     start with a lowercase word
     e.g : name, age, _name, _age, name_age, name_age_, name_age_1, name_age_2
 =end
+
 name = 'caz'
 age = 30
 gpa = 3.5
@@ -27,3 +29,22 @@ puts greeting[0]
 puts greeting.include? "llo"
 puts greeting.include? "z"
 puts greeting[1, 3]
+
+# numbers
+puts 2* 3 # basic math operations: +, -, *, /, %
+puts 2**3 # exponentiation
+puts 10%3 # modulus
+puts (1+2)*3 # order of operations
+puts 10/3.0 # int's and doubles
+
+num = 10
+num +=100 # +=, -=, *=, /=, %=
+puts num
+
+num= -36.8
+puts num.abs() # absolute value
+puts num.round() # round to nearest integer
+
+# Math class has useful  math methods
+puts Math.sqrt(144)
+puts Math.log(0)
