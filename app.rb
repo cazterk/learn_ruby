f1da1ff041f7ae8db3cb2a6689e1a5b2368ba1ab
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2022-03-28 13:20:06 +0200

    initail commit

---
+++ app.rb
@@ -0,0 +1,5 @@
+# printing
+
+puts "Hello"
+print "World!"
+puts "!"
---
+++ variables.rb
@@ -0,0 +1,21 @@
+# variables 
+=begin 
+    Variables are used to store data.
+    Variables are declared with the keyword:
+    names are case sensitive.
+    names can be any combination of letters, numbers, and underscores.
+    start with a letter or an underscore.
+    start with a lowercase word
+    e.g : name, age, _name, _age, name_age, name_age_, name_age_1, name_age_2
+=end
+name = 'caz'
+age = 30
+gpa = 3.5
+is_tall = true
+
+name = 'terk'
+
+puts "your name is #{name}"
+puts 'your name is ' + name
+
+# strings
